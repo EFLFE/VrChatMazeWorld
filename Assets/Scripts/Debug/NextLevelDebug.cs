506fdd011f8b111c1022b3ea25dbea31bd481da8
EFLFE <0eflfe0@gmail.com>
2024-06-09 13:51:08 +0300

    next level toggle

---
+++ Assets/Scripts/Debug/NextLevelDebug.cs
@@ -0,0 +1,12 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class NextLevelDebug : UdonSharpBehaviour {
+    [SerializeField] private MazeController controller;    
+
+    public override void Interact() {
+        base.Interact();
+        controller.NextLevelMaster();
+    }
+}
